0bc6234b8998d429a2a2c1fbebbbb41486df0e95
evtepo <andrey_laguta00@mail.ru>
2024-06-19 21:54:29 +1000

    fix spaces

--- easy/Remove Linked List Elements.py
+++ easy/Remove Linked List Elements.py
@@ -3,6 +3,8 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution:
     def removeElements(self, head: ListNode | None, val: int) -> ListNode | None:
         fake = ListNode(0)
