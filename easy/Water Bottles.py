aaa4b58b14705e8698950d7f8fe10320c388c6d5
evtepo <andrey_laguta00@mail.ru>
2024-07-07 12:05:43 +1000

    Water Bottles

---
+++ easy/Water Bottles.py
@@ -0,0 +1,6 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        return numBottles + (numBottles - 1) // (numExchange - 1)
+
+
+print(Solution().numWaterBottles(9, 3))
